9649f1bdf0020eda8c793b6f73400a63d078d2d6
mohamedmamdouh9 <mohamed.khalil@dxify.net>
2025-08-26 14:14:00 +0200

    Add project files.

---
+++ Ecom.Infrastructure/InfrastructureRegisteration.cs
@@ -0,0 +1,33 @@
+﻿using Ecom.Core.Interfaces;
+using Ecom.Infrastructure.Data;
+using Ecom.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure
+{
+    public static class InfrastructureRegisteration
+    {
+        public static IServiceCollection inftrastructureConfiguration(this IServiceCollection services , IConfiguration configuration)
+        {
+            services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+            //services.AddScoped<ICategoryRepository, CategoryRepository>();
+            //services.AddScoped<IProductRepository, ProductRepository>();
+            //services.AddScoped<IPhotoRepository, PhotoRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddDbContext<AppDbContext>(op =>
+            {
+                op.UseSqlServer(configuration.GetConnectionString("Ecom"));
+            });
+
+            return services;
+        }
+    }
+}
